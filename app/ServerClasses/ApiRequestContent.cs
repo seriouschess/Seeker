73359818239ccb32ddf4e9c39bbf83a4008f5d99
seriouschess <rehayemb@gmail.com>
2021-02-02 16:53:09 -0600

    project file restructure. added nunit test library

---
+++ app/ServerClasses/ApiRequestContent.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace Seeker.ServerClasses
+{
+    public class ApiRequestContent
+    {
+        private List<KeyValuePair<string,string>> _contentValues;
+        public ApiRequestContent(){
+            _contentValues = new List<KeyValuePair<string,string>>();
+        }
+        public void Add(string key, string value){
+            _contentValues.Add(new KeyValuePair<string,string>(key, value));
+        }
+
+        public FormUrlEncodedContent Export(){
+            return new FormUrlEncodedContent( _contentValues );
+        }
+    }
+}
---
+++ app/ServerClasses/Scanner.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+
+namespace Seeker.ServerClasses
+{
+    public class Scanner
+    {
+        public Scanner(){
+
+        }
+
+        //returns a percentage that the keywords were featured in the content
+        //100% means every word in the content was a keyword
+        public double ScanString(string content, List<string> keywords){
+            double output_percentage = 0;
+            int word_count = 0;
+            int flagged_keyword_count = 0;
+            bool reading_word = false;
+            char current_letter;
+            string current_word = "";
+            for(int x=0; x<content.Length; x++){
+                current_letter = content[x];
+                if(reading_word == false && current_letter !=  ' '){
+                    reading_word = true;
+                    word_count += 1;
+                    System.Console.WriteLine("new word");
+                }
+
+                if( current_letter == ' ' ){
+                    reading_word = false;
+                    current_word = "";
+                }else if(current_letter != '.'){
+                    current_word += current_letter;
+
+                    foreach(string keyword in keywords){
+                        if(keyword == current_word){
+                        flagged_keyword_count += 1; 
+                        break; 
+                        }
+                    }
+                    reading_word = true;
+                }                
+            }
+
+            output_percentage = (double)flagged_keyword_count/word_count;
+            return output_percentage;
+        }
+    }
+}
