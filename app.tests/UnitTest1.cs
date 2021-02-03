3e2ee046fb9a858f77aaf58a9f0b2201ca357647
seriouschess <rehayemb@gmail.com>
2021-02-03 16:56:55 -0600

    added unit testing for scanner module

--- app.tests/UnitTest1.cs
+++ app.tests/UnitTest1.cs
@@ -1,9 +1,13 @@
+using System.Collections.Generic;
 using NUnit.Framework;
+using Seeker.ServerClasses;
 
 namespace app.tests
 {
     public class Tests
     {
+        public Scanner _scannerModule = new Scanner();
+
         [SetUp]
         public void Setup()
         {
@@ -12,7 +16,24 @@ namespace app.tests
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            List<string> keywords = new List<string>(){
+                "cat"
+            };
+            string content = "The cat in the hat had a mat where he sat.";
+            double one_eleventh = (double)1/11;
+            Assert.AreEqual(one_eleventh, _scannerModule.ScanString( content, keywords ));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            List<string> keywords = new List<string>(){
+                "thrill",
+                "mill"
+            };
+            string content = "Bill got his thrill at the mill getting his fill";
+            double two_elevenths = (double)2/10;
+            Assert.AreEqual(two_elevenths, _scannerModule.ScanString( content, keywords ));
         }
     }
 }
